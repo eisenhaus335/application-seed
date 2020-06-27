65267be11ecc49ce5fb8636f9f478242b99f62b1
eisenhaus335 <anon@anon.com>
2020-06-27 22:00:48 +0700

    help

--- src/lib.rs
+++ src/lib.rs
@@ -139,7 +139,7 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 
 fn view(model: &Model) -> Node<Msg> {
     div![
-        "I'm a placeholder"
+        "I'm a second placeholder"
     ]
 }
 
