ab53fc0f5e6fc0ec0e2e987d6af09a5d8f819311
eisenhaus335 <anon@anon.com>
2020-06-27 21:37:00 +0700

    help

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #![allow(clippy::wildcard_imports)]
-
+// TODO: Remove
+#![allow(dead_code, unused_variables)]
+use std::collections::BTreeMap;
+use ulid::Ulid;
 use seed::{prelude::*, *};
 
-type ID ;
 struct Todo {
     id: Ulid,
     title: String,
@@ -16,30 +18,120 @@ enum Filter {
 }
 
 struct SelectedTodo {
-    id: ID,
+    id: Ulid,
     title: String,
     input_element: ElRef<web_sys::HtmlInputElement>,
 }
+
 struct Model {
-    todos: Vec<Todo>,
+    todos: BTreeMap<Ulid, Todo>,
     new_todo_title: String,
-    selected_todo: Option<SelectedTodo>
+    selected_todo: Option<SelectedTodo>,
+    filter: Filter,
+    base_url: Url,
 }
 
+impl Model {
+    fn add_mock_data(mut self) -> Self {
+        let (id_a, id_b) = (Ulid::new(), Ulid::new());
+
+        self.todos.insert(id_a, Todo {
+            id: id_a,
+            title: "I'm todo A".to_owned(),
+            completed: false,
+        });
+
+        self.todos.insert(id_a, Todo {
+            id: id_b,
+            title: "I'm todo B".to_owned(),
+            completed: false,
+        });
+
+        self.new_todo_title = "I'm a new todo title".to_owned();
+        
+        self.selected_todo = Some(SelectedTodo {
+            id: id_b,
+            title: "I'm better todo B".to_owned(),
+            input_element: ElRef::new()
+        });
+
+        self
+    }
+}
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    
+    Model {
+        todos: BTreeMap::new(),
+        new_todo_title: String::new(),
+        selected_todo: None,
+        filter: Filter::All,
+        base_url: Url::new(),
+    }.add_mock_data()
 }
 
-
-#[derive(Copy, Clone)]
 enum Msg {
-    Increment,
-}
+    UrlChanged(subs::UrlChanged),
+    NewTodoTitleChanged(String),
+ 
+    // ------ Basic Todo operations ------
+ 
+    CreateTodo,
+    ToggleTodo(Ulid),
+    RemoveTodo(Ulid),
+    
+    // ------ Bulk operations ------
+ 
+    CheckOrUncheckAll,
+    ClearCompleted,
+    
+    // ------ Selection ------
+ 
+    SelectTodo(Option<Ulid>),
+    SelectedTodoTitleChanged(String),
+    SaveSelectedTodo,
+ }
 
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => *model += 1,
+        Msg::UrlChanged(subs::UrlChanged(url)) => {
+            log!("UrlChanged", url);
+        }
+        Msg::NewTodoTitleChanged(title) => {
+            log!("NewTodoTitleChanged", title);
+        }
+    
+        // ------ Basic Todo operations ------
+
+        Msg::CreateTodo => {
+            log!("CreateTodo");
+        }
+        Msg::ToggleTodo(id) => {
+            log!("ToggleTodo");
+        }
+        Msg::RemoveTodo(id) => {
+            log!("RemoveTodo");
+        }
+        
+        // ------ Bulk operations ------
+
+        Msg::CheckOrUncheckAll => {
+            log!("CheckOrUncheckAll");
+        }
+        Msg::ClearCompleted => {
+            log!("ClearCompleted");
+        }
+        
+        // ------ Selection ------
+
+        Msg::SelectTodo(opt_id) => {
+            log!("SelectTodo", opt_id);
+        },
+        Msg::SelectedTodoTitleChanged(title) => {
+            log!("SelectedTodoTitleChanged", title);
+        },
+        Msg::SaveSelectedTodo => {
+            log!("SaveSelectedTodo");
+        }
     }
 }
 
@@ -47,9 +139,7 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 
 fn view(model: &Model) -> Node<Msg> {
     div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
+        "I'm a placeholder"
     ]
 }
 
